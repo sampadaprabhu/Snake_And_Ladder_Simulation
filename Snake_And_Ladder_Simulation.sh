c4539c03f97fcde8317e62769566363416bebe70
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-01 11:17:54 +0530

    ADDED: Calculated Count Of Dice And Position Of Player Using Dictionary

--- Snake_And_Ladder_Simulation.sh
+++ Snake_And_Ladder_Simulation.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 echo "Welcome To Snake And Ladder Game"
 
+#Declaring Dictionary
+declare -A currentPositionDict
+
 #Constant Variable
 PLAYER_POSITION=0
 
 #Variables
 playerCurrentPosition=$PLAYER_POSITION
+countOfDice=0
 
 #Create A Function To Roll The Dice
 rollingDice()
@@ -21,44 +25,49 @@ checkForOptions()
 	echo "Option : $options"
 	case $options in
 			0)
-				playerCurrentPosition=$playerCurrentPosition
+				currentPositionDict[$countOfDice]=$playerCurrentPosition
 				;;
 			1)
-				ladder=$(( $playerCurrentPosition + $dice ))
-				playerCurrentPosition=$ladder
-				echo $ladder
+				playerCurrentPosition=$(( $playerCurrentPosition + $dice ))
+				currentPositionDict[$countOfDice]=$playerCurrentPosition
+				echo $playerCurrentPosition
 				;;
 			2)
-				snake=$(( $playerCurrentPosition - $dice ))
-				playerCurrentPosition=$snake
-				echo $snake
-				if (( $snake < 0 ))
+				playerCurrentPosition=$(( $playerCurrentPosition - $dice ))
+				currentPositionDict[$countOfDice]=$playerCurrentPosition
+				echo $playerCurrentPosition
+				if (( $playerCurrentPosition < 0 ))
 				then
 					playerCurrentPosition=$PLAYER_POSITION
+					currentPositionDict[$countOfDice]=$playerCurrentPosition
 				fi
 				;;
 			*)
 				echo "No Matches Found!!"
 				;;
 	esac
+	echo "Position Of Player On $countOfDice Roll Dice:currentPositionDict[$countOfDice]"
 }
 tillWinningPosition()
 {
+	playerCurrentPosition=${currentPositionDict[$countOfDice]}
 	while (( $playerCurrentPosition <= 100 ))
 	do
+		((countOfDice++))
 		rollingDice
 		if (( $playerCurrentPosition == 100 ))
 		then
-				echo "Congratulations...You Won!!"
+				echo "Cogratulations...You Won!!"
 				break
 		elif (( $(( $playerCurrentPosition + $dice)) > 100 ))
 		then
-				playerCurrentPosition=$playerCurrentPosition
+				currentPositionDict[$countOfDice]=${currentPositionDict[$(($countOfDice - 1))]}
 		else
-		checkForOptions
+				checkForOptions
 		fi
 		echo
 		echo
 	done
 }
+currentPositionDict=([0]=0)
 tillWinningPosition
