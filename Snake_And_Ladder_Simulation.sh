d39dee424d81f10cb010c011276eed11b2bb7e3e
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-01 09:48:57 +0530

    ADDED: rollingDice() Function To Display Dice Output

--- Snake_And_Ladder_Simulation.sh
+++ Snake_And_Ladder_Simulation.sh
@@ -4,3 +4,11 @@ echo "Welcome To Snake And Ladder Game"
 #Constant Variable
 PLAYER_POSITION=0
 
+#Create A Function To Roll The Dice
+rollingDice()
+{
+	dice=$(( 1 + $((RANDOM%6)) ))
+	((countOfDice++))
+	echo "The number on Dice After Rolling the Dice is : $dice"
+}
+rollingDice
